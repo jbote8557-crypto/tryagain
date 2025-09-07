1ff6db2318a2733eab728175d56b7e5504aa5666
John <jbote8557@gmail.com>
2025-09-07 11:10:12 -0700

    trye

---
+++ other/overviewerConfig.js
@@ -0,0 +1,73 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "World"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1757261476",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        },
+        "attribution": null
+    },
+    "tilesets": [
+        {
+            "name": "Survival Daytime",
+            "zoomLevels": 11,
+            "defaultZoom": 1,
+            "maxZoom": 11,
+            "path": "survivalday",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "World",
+            "last_rendertime": 1756607945,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -862,
+                76,
+                14
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                -862,
+                76,
+                14
+            ],
+            "north_direction": 0
+        }
+    ]
+};
